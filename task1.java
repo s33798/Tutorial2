34533e3ce20fd863e88ea2c1cbd17080e8366eb4
Khryst <s33798@pjwstk.edu.pl>
2025-03-21 12:10:33 +0100

    First Task

---
+++ task1.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class task1 {
+    public static int[] EvenOdd(int[] array) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < array.length; i += 2) {
+            if (array[i] % 2 != 0) {
+                result.add(array[i]);
+            }
+        }
+        return result.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6};
+        int[] result = EvenOdd(array);
+
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+}
